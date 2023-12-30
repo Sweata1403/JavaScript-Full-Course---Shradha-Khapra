8411d577d36192e3d9dbf44dab009282840f69ae
Sweata Chakraborty <85730840+Sweata1403@users.noreply.github.com>
2023-12-31 00:17:57 +0530

    Add files via upload

---
+++ lecture2/practiceq1.js
@@ -0,0 +1,10 @@
+// Let's Practice
+// Qs1. Get user to input a number using prompt("Enter a number:"). Check if the number is
+// a multiple of 5 or not.
+
+let num =prompt("enter a number:");
+if (num % 5 === 0) {
+    console. log (num, " is multiple of 5");
+} else {
+    console. log(num, " is NOT a multiple of 5");
+}
