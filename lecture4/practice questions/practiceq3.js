d644f1ce8a673a70f7d9ee81789a4a2a2547d8f4
Sweata Chakraborty <85730840+Sweata1403@users.noreply.github.com>
2024-01-01 10:24:13 +0530

    Add files via upload

---
+++ lecture4/practice questions/practiceq3.js
@@ -0,0 +1,16 @@
+// Let's Practice
+// Qs. Create an array to store companies -> "Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"
+// a. Remove the first company from the array
+// b. Remove Uber & Add Ola in its place
+// c. Add Amazon at the end
+
+let companies=["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+// a. Remove the first company from the array
+// companies.shift();
+// console.log(companies);
+// b. Remove Uber & Add Ola in its place
+// companies.splice(2,1,"OLA");
+// console.log(companies);
+// c. Add Amazon at the end
+companies.push("Amazon");
+console.log(companies);
---
+++ lecture4/practice questions/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+companies.splice(6,0,"Aamazon");
+// console.log(companies);
